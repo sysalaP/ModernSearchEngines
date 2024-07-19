e62e28c4f0ae042adf2465e7d51a6674246f2be9
lauramoser <laura.moser2000@gmail.com>
2024-07-19 17:08:36 +0200

    gitignore

--- app.py
+++ app.py
@@ -18,24 +18,25 @@ def load_json_file(file_path):
     return {}
 
 # Get the Image of each document
-def find_image(index):
+def find_image(doc_number):
     supported_formats = ['.svg', '.png', '.jpg', '.jpeg']
     for fmt in supported_formats:
-        img_path = f'static/pictures/{index}{fmt}'
+        img_path = f'static/images/{doc_number}{fmt}'
         if os.path.exists(img_path):
             return img_path
-    return 'static/pictures/default_picture.jpg'
+    return 'static/images/default_picture.jpg'
 
 # Path categories
 categories = load_json_file('static/categories.json')
+document_index = load_json_file('index.json')
 
 # Get title and description
-def get_info(index):
-    json_path = f'static/documents/{index}.json'
+def get_info(doc_number):
+    json_path = f'static/documents/{doc_number}.json'
     data = load_json_file(json_path)
     title = data.get('title', 'No title')
     description = data.get('description', 'No description')
-    image_url = find_image(index)
+    image_url = find_image(doc_number)
     return title, description, image_url
 
 @app.route('/')
@@ -45,13 +46,8 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     query = request.args.get('query') if request.method == 'GET' else request.form['query']
-    ranklist = [
-    [0, 'https://en.wikipedia.org/wiki/Neckarfront', 4.320154330966258, 'neckarfront'],
-    [1, 'https://en.wikipedia.org/wiki/St._George%27s_Collegiate_Church,_T%C3%BCbingen', 4.294722935465357, 'ulrich'],
-    [2, 'https://en.wikivoyage.org/wiki/T%C3%BCbingen', 2.866682863719404, 'ulrich'],
-    [3, 'https://en.wikipedia.org/wiki/Friedrich_H%C3%B6lderlin', 2.1675640402040317, 'ulrich'],
-    [4, 'https://en.wikipedia.org/wiki/Stuttgart', 1.5731610178940094, 'ulrich']
-]
+    logging.debug(f"Received search query: {query}")
+    ranklist = retrieve(query, document_index)
     selected_filter = request.args.get('filter') if request.method == 'GET' else request.form.get('filter')
     filter_count = Counter([result[3] for result in ranklist])
     top_filters = [word for word, count in filter_count.most_common(5)]
@@ -60,12 +56,12 @@ def search():
     for result in ranklist:
         if selected_filter and result[3] != selected_filter:
             continue
-        idx = result[0]
+        idx = result[4]
         title, description, image_url = get_info(idx)
         results.append({'url': result[1], 'title': title, 'snippet': description, 'image': image_url})
+        logging.debug(f"Appended result: {results[-1]}")
 
     return render_template('results.html', query=query, results=results, top_filters=top_filters, categories=categories)
-
 
 @app.route('/filter', methods=['POST'])
 def filter():
